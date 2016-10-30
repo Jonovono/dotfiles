ab47ffa6977113645e99d8a82e67efc708dc3d74
Jordan Howlett <jordan@Jordans-MacBook-Air.local>
2016-10-30 19:25:48 -0400

    Got kwm to work. Still much to do

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,38 +1,46 @@
--- A global variable for the Hyper Mode
-k = hs.hotkey.modal.new({}, "F17")
+-- My own config file to get the Hyper Key working again with my current setup. 
+-- All credit goes to @prenagha and @ttscoff for their awesome original code that I tweaked for my own devices. 
+-- Credit 1: https://gist.github.com/ttscoff/cce98a711b5476166792d5e6f1ac5907
+-- Credit 2: https://gist.github.com/prenagha/1c28f71cb4d52b3133a4bff1b3849c3e
 
--- HYPER+E: Act like ⌃e and move to end of line.
-efun = function()
-  hs.eventtap.keyStroke({'⌃'}, 'e')
-  k.triggered = true
-end
-k:bind({}, 'e', nil, efun)
+-- A global variable for the sub-key Hyper Mode
 
--- HYPER+A: Act like ⌃a and move to beginning of line.
-afun = function()
-  hs.eventtap.keyStroke({'⌃'}, 'a')
-  k.triggered = true
-end
-k:bind({}, 'a', nil, afun)
+-- k = hs.hotkey.modal.new({}, 'F18')
 
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
-end
+-- k:bind({}, 'm', nil, function() hs.eventtap.keyStroke({"cmd","alt","shift","ctrl"}, 'm') 
+--     hs.alert.show("Hello World!")
+-- end)
 
--- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
-end
 
-k:bind({}, "w", function()
-  hs.alert.show("Hello World!")
-end)
+-- -- Hyper-key for all the below are setup somewhere... Usually Keyboard Maestro or similar. Alfred doesn't handle them very well, so will set up separate bindings for individual apps below.
 
--- Bind the Hyper key
-f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+-- hyperBindings = {'c','m','a','b','d','e','f','g','h','i','j','k','l','m','n','p','q','r','t','1','2','3','4','5','6','7','8','9','0','d','g','s','f','TAB','v','b','O','-','s'}
+
+-- for i,key in ipairs(hyperBindings) do
+--   k:bind({}, key, nil, function() hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, key)
+--     k.triggered = true
+--   end)
+-- end
+
+-- -- Enter Hyper Mode when F18 is pressed
+
+-- pressedF18 = function()
+--   k.triggered = false
+--   k:enter()
+-- end
+
+-- -- Leave Hyper Mode when F18 is pressed,
+-- --   send ESCAPE if no other keys are pressed.
+
+-- releasedF18 = function()
+--   k:exit()
+--   if not k.triggered then
+--     hs.eventtap.keyStroke({}, 'ESCAPE')
+--   end
+-- end
+
+-- -- Bind the Hyper key
+
+-- f19 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+
+hs.alert.show('Config loaded')
--- app.sh
+++ app.sh
@@ -10,6 +10,7 @@ brew cask install --appdir="/Applications" fantastical
 brew cask install --appdir="/Applications" backblaze
 brew cask install --appdir="/Applications" flux
 brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" hyper
 
 brew tap d12frosted/emacs-plus
 brew install emacs-plus
