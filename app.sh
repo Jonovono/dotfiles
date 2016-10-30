c7eb2b21f6bcba5f5a0b0dd4935a88e3eabd3cab
Jordan Howlett <jordan@Jordans-MacBook-Air.local>
2016-10-30 11:43:51 -0400

    INIT

---
+++ app.sh
@@ -0,0 +1,19 @@
+brew cask install --appdir="/Applications" dashlane
+brew cask install --appdir="/Applications" aerial
+brew cask install --appdir="/Applications" qutebrowser
+brew cask install --appdir="/Applications" bartender
+brew cask install --appdir="/Applications" pomotodo
+brew cask install --appdir="/Applications" google-photos-backup
+brew cask install --appdir="/Applications" dayone
+brew cask install --appdir="/Applications" rescuetime
+brew cask install --appdir="/Applications" fantastical
+brew cask install --appdir="/Applications" backblaze
+brew cask install --appdir="/Applications" flux
+
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+brew linkapps emacs-plus
+
+brew install koekeishiya/kwm/kwm
+brew services start kwm
+
