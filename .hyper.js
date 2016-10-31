b8e2115d9d18bd1683daaa12fd7c26ccd9fe4db5
Jordan Howlett <jordan@Jordans-MacBook-Air.local>
2016-10-30 21:27:40 -0400

    Remove crazy hyper mode

--- .hyper.js
+++ .hyper.js
@@ -81,7 +81,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperyellow', 'hyperpower'],
+  plugins: ['hyperyellow'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
